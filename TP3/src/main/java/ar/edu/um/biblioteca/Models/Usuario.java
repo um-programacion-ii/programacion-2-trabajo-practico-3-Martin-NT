9800154d9fb8fe78196326897c460207118b498c
Martin-NT <martinnavarro1503@gmail.com>
2025-04-29 12:57:27 -0300

    Implementación de la clase Usuario #13

---
+++ TP3/src/main/java/ar/edu/um/biblioteca/Models/Usuario.java
@@ -0,0 +1,19 @@
+package ar.edu.um.biblioteca.Models;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class Usuario {
+    private String nombre;
+    private String apellido;
+    private List<Prestamo> historialPrestamos = new ArrayList<>();
+
+    //Constructor
+    public Usuario(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
